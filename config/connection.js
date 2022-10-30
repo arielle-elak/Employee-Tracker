d42adb99607dd12d488c95d3b540cabd51417c3c
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-10-30 17:55:50 -0600

    Initialized and testing initial connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 // Initialize connection to server using variables from .env file
 
 require('dotenv').config();
+const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection(
+  {
     host: process.env.DB_NAME,
     user: process.env.DB_USER,
     database: process.env.DB_PASSWORD
-});
+  },
+  console.log('Connected to the employees_db database.')
+);
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-const mysql = require('mysql2');
+const express = require('express');
+// Import and require mysql2
 
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Import connection settings from config/connection
+const connection = require('./config/connection');
