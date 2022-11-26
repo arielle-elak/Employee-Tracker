0800bc8f7c544f97434bb56e66ab741cb3304d4e
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-11-25 20:35:27 -0700

    Still getting 1452 error code, calling employee id constraint employee_ibfk_2

--- db/schema.sql
+++ db/schema.sql
@@ -18,8 +18,8 @@ CREATE TABLE role(
         ON DELETE SET NULL
 );
 
-CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
@@ -27,7 +27,7 @@ CREATE TABLE employee(
     FOREIGN KEY (role_id)
         REFERENCES role(id)
         ON DELETE SET NULL,
-    FOREIGN KEY (manager_id)
+    FOREIGN KEY(manager_id)
         REFERENCES employee(id)
         ON DELETE SET NULL
 );
