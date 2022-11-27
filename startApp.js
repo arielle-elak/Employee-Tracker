30e9e4555acf553924bcd248005f4a2d349999af
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-11-26 21:55:48 -0700

    Testing of importing prompts and using inquirer

--- startApp.js
+++ startApp.js
@@ -1,11 +1,31 @@
 // Import utilities for asking prompts.
-const prompts = require("./utils/prompts");
+const {
+  topMenuPrompts,
+  viewPrompts,
+  viewSearchPrompts,
+  addPrompts,
+  addDepPrompts,
+  addEmpPrompts,
+  addRolePrompts,
+  updatePrompts,
+  updateDepPrompts,
+  updateEmpPrompts,
+  updateEmpFName,
+  updateEmpLName,
+  updateEmpDep,
+  updateEmpRole,
+  updateEmpMan,
+  updateEmpSal,
+} = require("./utils/prompts");
 
 // Import inquirer
 const inquirer = require("inquirer");
 
 const startApp = () => {
-    console.log(`Welcome to the Employee Tracker!`);
+  console.log(`Welcome to the Employee Tracker!`);
+  inquirer.prompt(topMenuPrompts).then((answers) => {
+    console.log(answers);
+  });
 };
 
 module.exports = startApp;
