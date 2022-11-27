c386b4567ce361ee5d205dcab282468f1cb0d64c
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-11-26 19:35:01 -0700

    Installed nodemon for server monitoring and establishing connection using normal .env injections

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
+// Import express
 const express = require('express');
-const routes = require('./routes');
+
+// Connection config import
 const connection = require('./config/connection');
 
+// Import and require mysql2
+const mysql = require('mysql2');
+
+// Use app to say when express needs to be used
 const app = express();
+// Designate port for express (either from .env or 3001 by default)
 const PORT = process.env.PORT || 3001;
 
+// Express middleware to interpret json input
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
-app.use(routes);
-
-// turn on connection to db and server
-connection.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// Connect to database
+const db = mysql.createConnection (
+  {
+    host: process.env.DB_HOST || 'localhost',
+    // MySQL username,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME || 'employees_db'
+  },
+  console.log(`Welcome to the Employee Tracker!`)
+);
