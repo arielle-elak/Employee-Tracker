3e252c834d3cecf2db0efcd4c711f7f9c80bc774
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-11-27 22:11:03 -0700

    Testing passing answer through functions

--- startApp.js
+++ startApp.js
@@ -1,31 +1,19 @@
 // Import utilities for asking prompts.
-const {
-  topMenuPrompts,
-  viewPrompts,
-  viewSearchPrompts,
-  addPrompts,
-  addDepPrompts,
-  addEmpPrompts,
-  addRolePrompts,
-  updatePrompts,
-  updateDepPrompts,
-  updateEmpPrompts,
-  updateEmpFName,
-  updateEmpLName,
-  updateEmpDep,
-  updateEmpRole,
-  updateEmpMan,
-  updateEmpSal,
-} = require("./utils/prompts");
+const { topMenuPrompts } = require("./utils/prompts");
 
 // Import inquirer
 const inquirer = require("inquirer");
 
+// Start the app
 const startApp = () => {
   console.log(`Welcome to the Employee Tracker!`);
-  inquirer.prompt(topMenuPrompts).then((answers) => {
-    console.log(answers);
+  inquirer.prompt(topMenuPrompts).then((answer) => {
+      routeApp(answer.topMenu);
   });
+};
+
+const routeApp = (route) => {
+  console.log("App routed with " + route);
 };
 
 module.exports = startApp;
